b0285d21c59dddd8a8b033ba9b0467bcfcbf7238
C0823G1-nguyentrucvi <nguyentrucvi10112004@gmail.com>
2023-11-03 21:28:37 +0700

    nop bai

--- module_2/src/ss1_introduction_to_java/bai_tap/NumbersIntoWords.java
+++ module_2/src/ss1_introduction_to_java/bai_tap/NumbersIntoWords.java
@@ -11,10 +11,14 @@ public class NumbersIntoWords {
         String[] array1 = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen"};
         if (number >= 0 && number < 14) {
             System.out.println(array1[number]);
-        }else if (number > 13 && number < 20) {
-                System.out.printf("%steen", array1[number - 10]);
-        }else if(number >=20&&number<100){
-            System.out.printf("");
+        } else if (number > 13 && number < 20) {
+            System.out.printf("%s teen", array1[number - 10]);
+        } else if (number >= 20 && number < 100) {
+            System.out.printf("%s ty", array1[number / 10]);
+        } else if (number >= 100 && number < 1000) {
+            System.out.printf("%s hundred", array1[number / 100]);
+        } else if (number > 20 && number <= 99) {
+            System.out.printf("%s ty ");
         }
     }
 }
