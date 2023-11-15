c713140fe39d554f44de978f877f2b2753aead4d
C0823G1-nguyentrucvi <nguyentrucvi10112004@gmail.com>
2023-11-15 11:45:02 +0700

    nop bai

--- module_2/src/ss7_mvc/TennisGame.java
+++ module_2/src/ss7_mvc/TennisGame.java
@@ -3,71 +3,83 @@ package ss7_mvc;
 
 public class TennisGame {
 
+    static final String LOVE = "Love";
 
-    public static String getScore( int point1, int point2) {
-        final String LOVE = "Love";
-        final String Fifteen="Fifteen";
-        final String Thirty="Thirty";
-        final String Forty="Forty";
-        final String All="_All";
+    static final String FIFTEEN = "Fifteen";
+    static final String THIRTY = "Thirty";
+    static final String FORTY = "Forty";
+    static final String ALL = "_All";
 
-        String score="";
-        int tempScore;
-        if (point1 == point2) {
-            switch (point1) {
-                case 0:
-                    score = LOVE+All;
-                    break;
-                case 1:
-                    score =Fifteen+All ;
-                    break;
-                case 2:
-                    score = Thirty+All;
-                    break;
-                case 3:
-                    score = Forty+All;
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        } else if (point1 >= 4 || point2 >= 4) {
-            int minusResult = point1 - point2;
-            if (minusResult == 1) {
-                score = "Advantage TrucVi";
-            } else if (minusResult == -1) {
-                score = "Advantage TranHong";
-            } else if (minusResult >= 2) {
-                score = "Win for TrucVi";
-            } else {
-                score = "Win for TranHong";
-            }
+    public static String getScore(int point1, int point2) {
+        String core = "";
+        int tempScore = 0;
+        if (point1 >= 4 || point2 >= 4) {
+            return temp(point1, point2);
+        } else if (point1 == point2) {
+            return tennis(point1);
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = point1;
-                else {
-                    score = "-";
-                    tempScore = point2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score =LOVE;
-                        break;
-                    case 1:
-                        score = Fifteen;
-                        break;
-                    case 2:
-                        score = Thirty;
-                        break;
-                    case 3:
-                        score = Forty;
-                        break;
-                }
-            }
+            return tennisS(point1,point2);
         }
-        return score;
     }
 
 
+    public static String tennisS(int tempScore, int point1) {
+        for (int i = 1; true; i++) {
+            if (i == 1) {
+                tempScore = point1;
+            } else {
+                return "-";
+            }
+            switch (tempScore) {
+                case 0:
+                    return LOVE;
+
+                case 1:
+                    return FIFTEEN;
+
+                case 2:
+                    return THIRTY;
+
+                case 3:
+                    return FORTY;
+
+            }
+        }
+    }
+    public static String tennis ( int point1){
+
+        switch (point1) {
+            case 0:
+                return LOVE + ALL;
+            case 1:
+                return FIFTEEN + ALL;
+
+            case 2:
+                return THIRTY + ALL;
+
+            case 3:
+                return FORTY + ALL;
+
+            default:
+                return "Deuce";
+
+        }
+    }
+    public static String temp ( int point1, int point2){
+        int minusResult = point1 - point2;
+        if (minusResult == 1) {
+            return "Advantage TrucVi";
+        } else if (minusResult == -1) {
+            return "Advantage TranHong";
+        } else if (minusResult >= 2) {
+            return "Win for TrucVi";
+        } else {
+            return "Win for TranHong";
+        }
+
+    }
 }
+
+
+
+
